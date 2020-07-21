f5fb64dc6a3c8555f8b2049c1a6efc3d61443099
fortes.huang <tokiya.kagami@gmail.com>
2020-07-21 12:24:03 +0800

    setup vue config

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/styles/style.scss';
 
 Vue.config.productionTip = true;
 Vue.prototype.$api = api;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div class="row horizontal v_center">
+      <span>01</span><span>02</span><span>03</span><span>04</span>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
